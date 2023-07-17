58213e6b790d9f2fec785fadf952e69813e9fde7
Reshma <kshaikreshma98@gmail.com>
2023-07-17 17:18:26 +0100

    feat(master): adding Paloma logo and background image with style - Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import PalomaSVG from "./images/Group 9.svg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div className="fixed-left">
+        <div class="paloma-svg">
+          <div>
+            <img src={PalomaSVG} alt="React Logo" />
+          </div>
+        </div>
+      </div>
+      <div class="rotated-text">
+        <ul>
+          <li>
+            <a href="#">HEAR</a>
+          </li>
+          <li>
+            <a href="#">LATEST</a>
+          </li>
+          <li>
+            <a href="#">TOUR</a>
+          </li>
+          <li>
+            <a href="#">SEE</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+          <li>
+            <a href="#">WATCH</a>
+          </li>
+          <li>
+            <a href="#">PERSONAL</a>
+          </li>
+          <li>
+            <a href="#">AID</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
